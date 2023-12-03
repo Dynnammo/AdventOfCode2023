b8382dd7949b06297428f6d359f0dcd839b10754
THIVEND <baptiste.thivend@protonmail.com>
2023-12-03 18:24:31 +0100

    Day 1 part 2

--- adventofcode2023/day_1.py
+++ adventofcode2023/day_1.py
@@ -1,20 +1,62 @@
-import re
+import regex as re
 from utils import extract_lines
 
 def extract_digits(string):
     results = re.findall(r'\d', string)
     first_and_last = [results[0], results[-1]]
-    print(first_and_last)
+
     return first_and_last
+
+def extract_digits_improved(string):
+    numbers_dict ={
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }
+    # extract all strings that are keys of numbers_dict
+    results = re.findall(r'\d|' + '|'.join(numbers_dict.keys()), string.lower(), overlapped=True)
+
+    first = results[0]
+    last = results[-1]
+
+    if first in numbers_dict.keys():
+        first = numbers_dict[first]
+    if last in numbers_dict.keys():
+        last = numbers_dict[last]
+
+    first_and_last = [first, last]
+
+    print(''.join(first_and_last))
+    return first_and_last
+    # extract written numbers from string like one, two etc.
+
 
 def concatenate_digits(string):
     return ''.join(extract_digits(string))
+
+def concatenate_digits_improved(string):
+    return ''.join(extract_digits_improved(string))
 
 def sum_all_digits(list_of_strings):
     list_of_ints = [concatenate_digits(x) for x in list_of_strings]
     return sum([int(x) for x in list_of_ints])
 
+def sum_all_digits_improved(list_of_strings):
+    list_of_ints = [concatenate_digits_improved(x) for x in list_of_strings]
+    return sum([int(x) for x in list_of_ints])
+
+
 def day_1():
     print(sum_all_digits(extract_lines('inputs/day_1.txt')))
 
+def day_1_part_2():
+    print(sum_all_digits_improved(extract_lines('inputs/day_1.txt')))
+
 day_1()
+day_1_part_2()
--- tests/test_day_1.py
+++ tests/test_day_1.py
@@ -8,3 +8,27 @@ def test_extract_line_correctly():
 
 def test_sum_correctly():
     assert day_1.sum_all_digits(['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'tre7buchet']) == 142
+
+
+# Part 2
+def test_extract_line_correctly_part2():
+    assert day_1.concatenate_digits_improved('two1nine') == '29'
+    assert day_1.concatenate_digits_improved('eightwothree') == '83'
+    assert day_1.concatenate_digits_improved('abcone2threexyz') == '13'
+    assert day_1.concatenate_digits_improved('xtwone3four') == '24'
+    assert day_1.concatenate_digits_improved('4nineeightseven2') == '42'
+    assert day_1.concatenate_digits_improved('zoneight234') == '14'
+    assert day_1.concatenate_digits_improved('7pqrstsixteen') == '76'
+
+def test_sum_correctly_part2():
+    assert day_1.sum_all_digits_improved(
+        [
+            'two1nine',
+            'eightwothree',
+            'abcone2threexyz',
+            'xtwone3four',
+            '4nineeightseven2',
+            'zoneight234',
+            '7pqrstsixteen',
+        ]
+    ) == 281
